12cf2c18a862d46357d63f7ad515890916bc75d4
Thales Silva <thales.wds2019@gmail.com>
2021-06-12 14:06:15 -0300

    Update the files to use react js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './global.css';
+import reportWebVitals from './reportWebVitals';
+
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route path='/Products' component={Products}/>
+            </Switch>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+
+export default function Home() {
+    return (
+        <div className='home-container'>Essa é a Home</div>
+    );
+}
